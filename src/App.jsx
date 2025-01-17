a9ccd8dbdb59be20b454cb0997636b7dfd208fd2
sulaymonn07 <i02245129@gmail.com>
2025-01-17 12:10:04 +0000

    React dars

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,7 @@
 import "./App.css";
 import Todo from "./components/todo";
 import Header from "./components/header";
-
-
+import { useState } from "react";
 
 // container
 //     header
@@ -15,21 +14,47 @@ import Header from "./components/header";
 // p
 
 function App() {
-  const todos = [
+  const [son, sonYangilash] = useState(0);
+  const [todo, setTodo] = useState("");
+
+  let todos = [
     {
       title: "Car",
     },
     {
       title: "Kun.Uz",
     },
-    
   ];
+
+  const addTodo = () => {
+    console.log(todo);
+
+    todos.push({ title: todo });
+  };
+
   return (
     <div>
-      <Header />
+      <div>
+        <span>
+          <button onClick={() => sonYangilash(son - 1)}>-</button>
+          <span>{son}</span>
+          <button onClick={() => sonYangilash(son + 1)}>+</button>
+        </span>
+      </div>
+      <header className="header">
+        <input
+          onChange={(e) => console.log(e.target.value)}
+          className="input"
+          type="text"
+          placeholder="Add Task"
+        />
+        <button onClick={addTodo} className="button">
+          Add
+        </button>
+      </header>
       <div className="todo-container">
         {todos.map((t) => {
-          return <Todo title={t.title}/>
+          return <Todo title={t.title} />;
         })}
       </div>
     </div>
